05ddcc775be8ad00e690d94298af2b859e5cdd56
Jeffrey Wienen <j.wienen@hotmail.com>
2026-03-24 15:57:05 +0100

    Fix url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.DESKTOP_BUILD ? "export" : "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000",
+  },
   images: {
     unoptimized: !!process.env.DESKTOP_BUILD,
     remotePatterns: [
